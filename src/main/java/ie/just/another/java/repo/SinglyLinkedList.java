61f90f981f7797ebd187ddf9d3165a0f84229988
marco.bartolini <marco.bartolini@workday.com>
2020-05-10 13:32:40 +0100

    exercising on linked list

--- src/main/java/ie/just/another/java/repo/SinglyLinkedList.java
+++ src/main/java/ie/just/another/java/repo/SinglyLinkedList.java
@@ -1,26 +1,48 @@
 package ie.just.another.java.repo;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class SinglyLinkedList<T> {
 
+    private int size = 0;
     private Node<T> head;
 
-    private static class Node<T> {
+    public static class Node<T> {
 
-        T data;
-        Node next;
+        private final T data;
+        private Node<T> next;
+        private int index;
 
-        // Constructor
         Node(T d) {
             data = d;
         }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        @Override
+        public String toString() {
+            return new StringBuilder("data: ").append(data).append(" index: ").append(index).toString();
+        }
     }
 
-    public SinglyLinkedList insert(T data) {
+    public SinglyLinkedList<T> addAll(Collection<T> collectionToAdd) {
+        for (T element : collectionToAdd) {
+            this.add(element);
+        }
+        return this;
+    }
 
-        Node<T> nodeToAdd = new Node(data);
+    public SinglyLinkedList<T> add(T data) {
+
+        Node<T> nodeToAdd = new Node<>(data);
 
         if (this.head == null) {
             this.head = nodeToAdd;
@@ -29,17 +51,61 @@ public class SinglyLinkedList<T> {
             while (last.next != null) {
                 last = last.next;
             }
-
+            nodeToAdd.index = last.index + 1;
             last.next = nodeToAdd;
-        }
 
+        }
+        size++;
         return this;
     }
 
-    public List<T> asList() {
-        return addToList(this.head, new ArrayList());
+    public SinglyLinkedList<T> clear() {
+        if (this.head != null) {
+            this.head = null;
+        }
+        size = 0;
+        return this;
     }
 
+    public T get(int index) {
+        Node<T> element = this.head;
+        while (element != null) {
+            if (element.index == index) {
+                return element.data;
+            }
+            element = element.next;
+        }
+        return null;
+    }
+
+    public int size() {
+//        this is easier to maintain, but it doesn't perform on large numbers due to the O(n) notation
+//        int size = 0;
+//        Node element = this.head;
+//        while(element != null) {
+//            element = element.next;
+//            size += 1;
+//        }
+        return size;
+    }
+
+    public T getHead() {
+        return head.data;
+    }
+
+    public void display() {
+        Node<T> element = this.head;
+        while (element != null) {
+            System.out.println(element);
+            element = element.next;
+        }
+    }
+
+    public List<T> asList() {
+        return addToList(this.head, new ArrayList<T>());
+    }
+
+    // just to exercise on recursion
     private List<T> addToList(Node<T> node, List<T> list) {
         if (node != null) {
             list.add(node.data);
--- src/test/java/test/ie/just/another/java/repo/SinglyLinkedListTest.java
+++ src/test/java/test/ie/just/another/java/repo/SinglyLinkedListTest.java
@@ -1,28 +1,125 @@
 package test.ie.just.another.java.repo;
 
 import ie.just.another.java.repo.SinglyLinkedList;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class SinglyLinkedListTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    public static SinglyLinkedList<String> init() {
+
+        SinglyLinkedList<String> testSinglyLinkedList = new SinglyLinkedList<>();
+
+        testSinglyLinkedList.add("first element");
+        testSinglyLinkedList.add("second element");
+        testSinglyLinkedList.add("third element");
+        testSinglyLinkedList.add("fourth element");
+
+        return testSinglyLinkedList;
+    }
+
     @Test
     public void singlyLinkedList() {
-        SinglyLinkedList<String> testSinglyLinkedList = new SinglyLinkedList();
-        testSinglyLinkedList.insert("first element");
-        testSinglyLinkedList.insert("second element");
-        testSinglyLinkedList.insert("third element");
-        testSinglyLinkedList.insert("fourth element");
 
-        List<String> testList =  testSinglyLinkedList.asList();
+        SinglyLinkedList<String> testSinglyLinkedList = init();
+
+        assertEquals(testSinglyLinkedList.get(0), "first element");
+        assertEquals(testSinglyLinkedList.get(1), "second element");
+    }
+
+    @Test
+    public void add() {
+
+        SinglyLinkedList<String> testSinglyLinkedList = init();
+
+        testSinglyLinkedList.add("last element");
+
+        assertEquals("last element", testSinglyLinkedList.get(4));
+        assertEquals(5, testSinglyLinkedList.size());
+    }
+
+    @Test
+    public void addAll() {
+
+        List<String> elements = new ArrayList<>();
+        elements.add("0");
+        elements.add("1");
+        elements.add("2");
+        elements.add("3");
+        elements.add("4");
+
+        SinglyLinkedList<String> testSinglyLinkedList = new SinglyLinkedList<>();
+        testSinglyLinkedList.addAll(elements);
+
+        assertEquals(5, testSinglyLinkedList.size());
+        assertEquals("0", testSinglyLinkedList.get(0));
+        assertEquals("4", testSinglyLinkedList.get(4));
+
+    }
+
+    @Test
+    public void clear() {
+
+        SinglyLinkedList<String> testSinglyLinkedList = init();
+        assertEquals(4, testSinglyLinkedList.size());
+
+        testSinglyLinkedList.clear();
+        assertEquals(0, testSinglyLinkedList.size());
+
+    }
+
+    @Test
+    public void size() {
+
+        SinglyLinkedList<String> testSinglyLinkedList = init();
+        assertEquals(4, testSinglyLinkedList.size());
+
+        testSinglyLinkedList.clear();
+        assertEquals(0, testSinglyLinkedList.size());
+
+    }
+
+
+    @Test
+    public void asList() {
+        List<String> testList = init().asList();
         assertEquals(testList.get(0), "first element");
         assertEquals(testList.get(1), "second element");
-        assertEquals(testList.size(),4);
+        assertEquals(testList.size(), 4);
+    }
 
-        testSinglyLinkedList.insert("last element");
-        assertEquals("last element", testSinglyLinkedList.asList().get(4));
+    @Test
+    public void display() {
+
+        SinglyLinkedList<String> testSinglyLinkedList = init();
+        testSinglyLinkedList.display();
+        assertEquals("data: first element index: 0\n" +
+                "data: second element index: 1\n" +
+                "data: third element index: 2\n" +
+                "data: fourth element index: 3\n", outContent.toString());
+
     }
 
 }
